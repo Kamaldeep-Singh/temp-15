ebdc991e6deb59ba0bf9c4231bd0088e218ecdd3
Kamaldeep Singh <ios.kamaldeep@gmail.com>
2015-02-11 16:04:31 -0800

    WeatherSDK small enhancement

---
+++ WeatherSDK/WMWeatherSDK.h
@@ -0,0 +1,18 @@
+//
+//  WMWeatherSDK.h
+//  WeatherSDK
+//
+//  Created by Kamaldeep on 2/11/15.
+//
+//
+
+#ifndef WeatherSDK_WMWeatherSDK_h
+#define WeatherSDK_WMWeatherSDK_h
+
+#import "WMParam.h"
+#import "WMTypes.h"
+#import "WMRequest.h"
+#import "WMCityWeather.h"
+#import "WMRequestProtocol.h"
+#import "WMCurrentWeatherQuery.h"
+#endif
